dfc054b87e977ae267cfd4cacf572cc31beeb999
Marla Schulz <marlaa.schulz@gmail.com>
2021-09-07 16:35:11 -0700

    Update factory

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :transaction do
+    result {'success'}
+    association :invoice, factory: :invoice_shipped
+  end
+
+  factory :transaction_failed, class: Transaction do
+    result {'failed'}
     association :invoice, factory: :invoice_shipped
   end
 end
