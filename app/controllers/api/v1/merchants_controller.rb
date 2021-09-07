bcf484591d129bb109a760cdd2a912464a4bc230
Marla Schulz <marlaa.schulz@gmail.com>
2021-09-07 12:51:54 -0700

    Fix for postman tests

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::MerchantsController <ApplicationController
   def index
     params[:per_page] = 20 if params[:per_page].nil?
-    params[:page] = 1 if params[:page].nil?
+    params[:page] = 1 if params[:page].nil? || params[:page].to_i <= 0
     merchants = Merchant.paginate(page: params[:page], per_page: params[:per_page])
+
     render json: MerchantSerializer.format_merchants(merchants)
   end
 
