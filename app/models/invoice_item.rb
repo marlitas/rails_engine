0bda021762bd3bbc69b20d8ead4d31bbc688cc8d
Marla Schulz <marlaa.schulz@gmail.com>
2021-09-07 16:34:53 -0700

    Add weekly revenue method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,13 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+
+  def self.weekly_revenue
+    joins(invoice: :transactions)
+    .where('invoices.status = ?', 'shipped')
+    .where('transactions.result = ?', 'success')
+    .select("SUM(invoice_items.unit_price * invoice_items.quantity) as revenue, date_trunc('week', invoices.updated_at) as week")
+    .group("week")
+    .order("week")
+  end
 end
